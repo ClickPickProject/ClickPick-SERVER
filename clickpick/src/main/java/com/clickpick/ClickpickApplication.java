109473325cacc8f06df8c708b8c8a4df084ea2ba
taemintaeminHyeon <taemin14215@gmail.com>
2024-02-17 15:17:42 +0900

    Make Project

---
+++ clickpick/src/main/java/com/clickpick/ClickpickApplication.java
@@ -0,0 +1,13 @@
+package com.clickpick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickpickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickpickApplication.class, args);
+	}
+
+}
---
+++ clickpick/src/test/java/com/clickpick/ClickpickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clickpick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickpickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
