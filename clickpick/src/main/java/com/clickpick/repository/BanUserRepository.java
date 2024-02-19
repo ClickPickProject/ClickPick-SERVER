cb1d3b8fc3886225a46abd335ac12dc7e453f2ee
taemintaeminHyeon <taemin14215@gmail.com>
2024-02-20 03:36:15 +0900

    [ADD] BanUser, Comment, Notice, Post, ReportComment, ReportPost Repository

--- clickpick/src/main/java/com/clickpick/repository/BanUserRepository.java
+++ clickpick/src/main/java/com/clickpick/repository/BanUserRepository.java
@@ -1,11 +1,48 @@
 package com.clickpick.repository;
 
+import com.clickpick.domain.BanUser;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class BanUserRepository {
     private final EntityManager em;
+
+
+    // 정지 유저 추가
+    public void save(BanUser banUser) {
+        em.persist(banUser);
+    }
+
+    //유저 ID를 통한 정지 유저 확인
+    public BanUser findById(String userId) {
+        return em.createQuery("select b from BanUser b where b.user.id =: userId", BanUser.class)
+                .setParameter("userId",userId)
+                .getSingleResult();
+    }
+
+    // 정지 시작일을 통한 정지 유저리스트 확인
+    public List<BanUser> findByStartDate(LocalDateTime startDate) {
+        return em.createQuery("select b from BanUser b where b.startDate =: startDate", BanUser.class)
+                .setParameter("startDate",startDate)
+                .getResultList();
+    }
+
+    // 정지 종료일을 통한 정지 유저리스트 확인
+    public List<BanUser> findByEndDate(LocalDateTime endDate) {
+        return em.createQuery("select b from BanUser b where b.endDate =: endDate", BanUser.class)
+                .setParameter("endDate",endDate)
+                .getResultList();
+    }
+
+    // 전체 정지 유저 검색
+
+    public List<BanUser> findAll() {
+        return em.createQuery("select b from  BanUser b",BanUser.class).getResultList();
+    }
 }
--- clickpick/src/main/java/com/clickpick/repository/CommentRepository.java
+++ clickpick/src/main/java/com/clickpick/repository/CommentRepository.java
@@ -1,11 +1,42 @@
 package com.clickpick.repository;
 
+import com.clickpick.domain.Comment;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class CommentRepository {
     private final EntityManager em;
+
+    // 댓글 저장
+    public void save(Comment comment){
+        em.persist(comment);
+    }
+
+
+    // 게시글 아이디를 통한 댓글 리스트 검색
+    public List<Comment> findByPostId(Long postId){
+        return em.createQuery("select c from Comment c where c.post.id =: postId",Comment.class)
+                .setParameter("postId",postId)
+                .getResultList();
+
+    }
+
+    // 유저 아이디를 통한 댓글 리스트 검색
+    public List<Comment> findByUserId(Long userId){
+        return em.createQuery("select c from Comment c where c.user.id =: userId",Comment.class)
+                .setParameter("userId",userId)
+                .getResultList();
+    }
+
+    //댓글 아이디를 이용한 해당 댓글 검색
+    public Comment findById(Long id){
+        return em.find(Comment.class,id);
+    }
+
+
 }
