46c57a734a6ee2b76944eb57e75e83168d82dfe4
taemintaeminHyeon <taemin14215@gmail.com>
2024-02-20 22:09:35 +0900

    [ADD] User Service

---
+++ clickpick/src/main/java/com/clickpick/service/UserService.java
@@ -0,0 +1,19 @@
+package com.clickpick.service;
+
+import com.clickpick.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+}
