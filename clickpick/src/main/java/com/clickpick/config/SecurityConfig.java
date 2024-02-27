f789ef70bc166edc3b5f8c3b0538d6031f87a716
taemintaeminHyeon <taemin14215@gmail.com>
2024-02-27 21:24:49 +0900

    [FIX] CORS 허용

--- clickpick/src/main/java/com/clickpick/config/SecurityConfig.java
+++ clickpick/src/main/java/com/clickpick/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
 
         http
                 .csrf((csrf) -> csrf.disable())
-                .cors((c) -> c.disable())
+                //.cors((c) -> c.disable())
                 .headers((headers) -> headers.disable());
 
 
